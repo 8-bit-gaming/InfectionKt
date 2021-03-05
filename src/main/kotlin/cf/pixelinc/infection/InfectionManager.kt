15945e37d848a50f0f2b2f5df09b26b773fa58dc
PixeL <officialpixelinc@gmail.com>
2021-03-05 15:18:09 -0600

    Make InfectionType better, main spawn infected entity function

--- src/main/kotlin/cf/pixelinc/infection/InfectionManager.kt
+++ src/main/kotlin/cf/pixelinc/infection/InfectionManager.kt
@@ -1,8 +1,15 @@
 package cf.pixelinc.infection
 
+import cf.pixelinc.InfectionPlugin
+import cf.pixelinc.entities.InfectedEntity
 import cf.pixelinc.infection.infections.AirborneInfection
 import cf.pixelinc.infection.infections.ZombieInfection
+import net.minecraft.server.v1_16_R3.EntityAnimal
+import org.bukkit.NamespacedKey
+import org.bukkit.craftbukkit.v1_16_R3.CraftWorld
+import org.bukkit.entity.Entity
 import org.bukkit.entity.EntityType
+import org.bukkit.persistence.PersistentDataType
 
 class InfectionManager {
     private val infections = ArrayList<BaseInfection>()
@@ -13,6 +20,21 @@ class InfectionManager {
 
     fun getInfection(type: EntityType): BaseInfection? {
         return infections.find { it.entity == type }
+    }
+
+    // Spawn an infected animal
+    fun spawnInfectedAnimal(entity : Entity, animal : EntityAnimal, infectionType : InfectionType) {
+        val infectedEntity = InfectedEntity(entity.location, animal.entityType)
+        val bukkitEntity: Entity = infectedEntity.bukkitEntity
+
+        bukkitEntity.persistentDataContainer.set(
+            NamespacedKey(InfectionPlugin.instance, "infected"),
+            PersistentDataType.INTEGER,
+            infectionType.value
+        )
+
+        entity.remove()
+        (entity.world as CraftWorld).handle.addEntity(infectedEntity)
     }
 
     init {
--- src/main/kotlin/cf/pixelinc/infection/InfectionType.kt
+++ src/main/kotlin/cf/pixelinc/infection/InfectionType.kt
@@ -1,8 +1,14 @@
 package cf.pixelinc.infection
 
-enum class InfectionType {
-    NONE,
-    ZOMBIE,
-    VAMPIRE,
-    AIR
+enum class InfectionType(val value : Int, val chance : Double) {
+
+    NONE(0, 0.0),
+    ZOMBIE(1, 0.4),
+    VAMPIRE(2, 0.2),
+    AIR(3, 0.5);
+
+    companion object {
+        private val map = values().associateBy(InfectionType::value)
+        fun fromInt(type : Int) = map[type]
+    }
 }
